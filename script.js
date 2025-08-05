69ed842edab0d1c661c622d9ac1d6a0ca68538fb
anram61 <anasrr2001@icloud.com>
2025-08-05 12:14:13 +0400

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,12 @@ function displayReport() {
 
     if (selectedCompany === 'Adnoc Offshore') {
       // Embed the PDF with no internal scroll (large height)
-      reportText.innerHTML = `
-        <iframe src="reports/offshore-report.pdf#toolbar=0" width="100%" height="1400px" style="border: none; overflow: hidden;"></iframe>
-      `;
+     reportText.innerHTML = `
+  <div class="pdf-container">
+    <iframe src="reports/offshore-report.pdf#toolbar=0"></iframe>
+  </div>
+`;
+
     } else if (selectedCompany === 'Year to date Average') {
       // Custom message for YTD average
       reportText.innerHTML = `
