7ca8963674eb70ab2976dc6752a88e5f6f7c31a1
anram61 <anasrr2001@icloud.com>
2025-08-05 12:25:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ function displayReport() {
   <iframe src="reports/offshore-report.pdf#toolbar=0" width="100%" height="1400px" style="border: none; overflow: hidden;"></iframe>
 `;
 
-
     } else if (selectedCompany === 'Year to date Average') {
       // Custom message for YTD average
       reportText.innerHTML = `
