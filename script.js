6aa461246f8fe4000d86dd4216bc024277151e26
anram61 <anasrr2001@icloud.com>
2025-08-13 12:30:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -78,8 +78,8 @@ async function renderPDF(pdfPath) {
         linkEl.target = '_blank';
         linkEl.style.position = 'absolute';
         linkEl.style.left = (rect[0] * scale / devicePixelRatio) + 'px';
-        const verticalOffset = window.innerWidth >= 769 ? 20 : 0;
-        linkEl.style.top = ((rect[1] * scale / devicePixelRatio) - verticalOffset) + 'px';
+        const verticalOffset = window.innerWidth >= 769 ? 50 : 0;
+        linkEl.style.top = ((pageContainer.clientHeight - (rect[3] * scale / devicePixelRatio)) - verticalOffset) + 'px';
         linkEl.style.width = ((rect[2] - rect[0]) * scale / devicePixelRatio) + 'px';
         linkEl.style.height = ((rect[3] - rect[1]) * scale / devicePixelRatio) + 'px';
         linkEl.style.zIndex = 10;
